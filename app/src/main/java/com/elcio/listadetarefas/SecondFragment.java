888a93779dfbee9f00741273b56c489604979b8a
Elcio Cestari Taira <elciotaira@gmail.com>
2020-09-22 21:24:38 -0400

    I added a listner to save's button to handler with the click when the user wish save a new data. In this the app still don't save nothing, this it is just to show a simple toast to make sure the button click it's working

--- app/src/main/java/com/elcio/listadetarefas/SecondFragment.java
+++ app/src/main/java/com/elcio/listadetarefas/SecondFragment.java
@@ -4,21 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class SecondFragment extends Fragment {
+    private EditText editName, editAge;
+    private Button btnSave;
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        editName = view.findViewById(R.id.editName);
+        editAge = view.findViewById(R.id.editAge);
+        btnSave  = view.findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(myBtnSaveListner());
+
+        return view;
     }
+
+    private View.OnClickListener myBtnSaveListner() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myCustomToast("salvo");
+            }
+        };
+    }
+
+    private void myCustomToast(String message) {
+        Toast.makeText(getActivity().getApplicationContext(),message, Toast.LENGTH_SHORT ).show();
+    }
+
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
